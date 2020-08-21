c79177668ebd4fe75ecd19b25d6ba8762def97b5
Puleeno Nguyen <puleeno@gmail.com>
2020-08-21 17:24:17 +0700

    Init composer project

---
+++ buckler.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Buckler Optimizer
+ * Plugin URI: https://github.com/ramphor/buckler
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ * Description: The WordPress Optimize Plugin: Speedup & Improve SEO
+ * Version: 1.0.0
+ * Tag: speed, seo, optimize, lighthouse
+ */
+
